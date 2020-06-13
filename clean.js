ddef2cfe22a50d8e1c183b497bb36f1b6c3f6f36
Jefferson <jeffe.chua@gmail.com>
2020-06-13 22:50:57 +0800

    Made autoclean not silly, i.e. much shorter and much better.

--- clean.js
+++ clean.js
@@ -3,54 +3,21 @@
 function autoClean(e) {
     maskContext.clearRect(0, 0, w2, h2);
     for (let i = 0; i < Reduction.vGridLines.length; i++) {
-        const lv = Reduction.vGridLines[i].llower - 1 - Reduction.vLower; // immediate left of grid line
-        const uv = Reduction.vGridLines[i].uupper + 1 - Reduction.vLower; // immediate right of grid line
-        let triggered = false;
-        let lu; // bottom of current triggered zone
-        let uu; // top of current triggered zone
-        let llv; // left of current triggered zone <= lu
-        let uuv; // right of current triggered zone >= uu
-
-        let u;
-        for (u = 0; u < Reduction.uSize; u++) {
+        const lv = Reduction.vGridLines[i].llower - 1 - Reduction.vLower; // immediate up of grid line
+        const uv = Reduction.vGridLines[i].uupper + 1 - Reduction.vLower; // immediate down of grid line
+        for (let u = 0; u < Reduction.uSize; u++) {
             if (Reduction.uGridLookup[u] == true)
                 continue;
-            let ltrig = Reduction.isBlack(u, lv);
-            let utrig = Reduction.isBlack(u, uv);
-            if (!triggered && (ltrig || utrig)) { // starting a triggered zone
-                triggered = true;
-                lu = u;
-                llv = lv;
-                uuv = uv;
-            } else if (triggered && !(ltrig || utrig)) { // closing a triggered zone
-                triggered = false;
-                uu = u - 1;
-                uSize = uu - lu + 1;
-                vSize = uuv - llv + 1;
-                if (Reduction.axis)
-                    maskContext.fillRect(lu, llv, uSize, vSize);
-                else
-                    maskContext.fillRect(llv, lu, vSize, uSize);
+            if (Reduction.isBlack(u, lv)) {
+                let v = lv;
+                while (Reduction.isBlack(u, v - 1)) v--;
+                maskContext.fillRect(u, v, 1, lv - v + 1);
             }
-            if (triggered) {
-                let v;
-                // walk towards -u until we find nonblack
-                for (v = lv; v >= 0 && Reduction.isBlack(u, v); v--) { } v++;
-                if (v < llv) llv = v;
-                // walk towards +u until we find nonblack
-                for (v = uv; v < Reduction.vSize && Reduction.isBlack(u, v); v++) { } v--;
-                if (v > uuv) uuv = v;
+            if (Reduction.isBlack(u, uv)) {
+                let v = uv;
+                while (Reduction.isBlack(u, v + 1)) v++;
+                maskContext.fillRect(u, uv, 1, v - uv + 1);
             }
-        }
-        if (triggered) { // cleanup
-            triggered = false;
-            uu = u - 1;
-            uSize = uu - lu + 1;
-            vSize = uuv - llv + 1;
-            if (Reduction.axis)
-                maskContext.fillRect(lu, llv, uSize, vSize);
-            else
-                maskContext.fillRect(llv, lu, vSize, uSize);
         }
     }
     recomputeReduction();
